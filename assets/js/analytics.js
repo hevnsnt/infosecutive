3e7c3f5478f8e2f5a2609ec172b46eb2165c709c
hevnsnt <hevnsnt@gmail.com>
2025-06-26 11:47:17 -0500

    removed swiper

---
+++ assets/js/analytics.js
@@ -0,0 +1,22 @@
+// assets/js/tracking.js
+
+  <!-- Google Tag Manager -->
+  <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+  new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+  j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+  'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','GTM-5R7JCTR7');</script>
+  <!-- End Google Tag Manager -->
+   <!-- Google tag (gtag.js) -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-VM7DSLMF77"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-VM7DSLMF77');
+  </script>
+  <!-- End Google tag (gtag.js) -->
+
+// Microsoft Clarity
+(function(c,l,a,r,i,t,y){…})(window, document, "clarity", "script", "YOUR_CLARITY_ID");
